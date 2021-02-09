4531b939c82537a499d1e0c7e127569db834c0f1
twolak88 <twolak88@gmail.com>
2021-02-09 17:44:07 +0100

    injecting LoggingService by constructor and use it in components

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,22 +1,25 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService]
 })
 export class AccountComponent implements OnInit {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.loggingService.logStatusChange(status);
+    // console.log('A server status changed, new status: ' + status);
   }
 }
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,14 +1,16 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
@@ -18,6 +20,7 @@ export class NewAccountComponent implements OnInit {
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
+    // console.log('A server status changed, new status: ' + accountStatus);
   }
 }
